e53a91b3a3c608a16966261b4c3952dd62dc4196
Liviu Cornea <liviucornea@rogers.com>
2019-03-02 11:01:38 -0500

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from './home/home.module';
+import {AppRoutingModule} from './app-routing.module';
+import {SharedModuleModule} from './shared-module/shared-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule.forRoot(),
+    SharedModuleModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
